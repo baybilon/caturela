fab16ccde0b1e4f2b055ccd991c247c8e0c61123
baybilon <baybilon_iqbal@hotmail.com>
2025-11-06 04:24:38 +0700

    add some

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Show popup when page loads
 window.onload = function () {
   document.getElementById("browser-popup").style.display = "flex";
 };
 
-// Close popup function
 function closePopup() {
   document.getElementById("browser-popup").style.display = "none";
 }
