4385a0b6fb2f60a9b7a79939ddda50231befca8f
baybilon <baybilon_iqbal@hotmail.com>
2025-11-05 18:47:03 +0700

    hello

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show popup when page loads
+window.onload = function () {
+  document.getElementById("browser-popup").style.display = "flex";
+};
+
+// Close popup function
+function closePopup() {
+  document.getElementById("browser-popup").style.display = "none";
+}
